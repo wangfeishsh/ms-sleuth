133b9b669f066445485f3f807ad90eb5a87d0c73
baochunyu <baochunyu@nonobank.com>
2016-10-12 16:13:00 +0800

    init

---
+++ src/main/java/com/bao/controller/TestController.java
@@ -0,0 +1,12 @@
+package com.bao.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by user on 16/10/12.
+ */
+
+@RestController
+public class TestController {
+
+}
