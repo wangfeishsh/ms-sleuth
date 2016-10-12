e08422950b3f236de77c1e82efacf4063d1cf04b
baochunyu <baochunyu@nonobank.com>
2016-10-12 20:59:50 +0800

    init

--- src/main/java/com/bao/controller/TestController.java
+++ src/main/java/com/bao/controller/TestController.java
@@ -1,12 +1,21 @@
 package com.bao.controller;
 
+import lombok.extern.slf4j.Slf4j;
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Created by user on 16/10/12.
  */
-
+@Slf4j
 @RestController
 public class TestController {
+    @RequestMapping("/index")
+    public String index() {
+        log.info("first");
+        return "Greetings from Spring Boot!";
+    }
 
 }
